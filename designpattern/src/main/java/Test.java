2d9235d04687c1c51716f2d7191959620da25e98
yuhao.zx <yuhao.zx@antfin.com>
2018-10-13 11:30:28 +0800

    f

---
+++ designpattern/src/main/java/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+
+/**
+ *
+ * @author yuhao.zx
+ * @version $Id: Test.java, v 0.1 2018年10月13日 上午11:23 yuhao.zx Exp $
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
